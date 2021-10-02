25cf9d7b6ee08a31c18c6272b9f6d07f60b77d8d
PradeepKumarHE <pradeepkumarhe1989@gmail.com>
2021-10-02 19:47:41 +0530

    Update AuditModel.java
    Alignment in online

--- src/main/java/com/pradeep/jparelation/domain/AuditModel.java
+++ src/main/java/com/pradeep/jparelation/domain/AuditModel.java
@@ -22,7 +22,8 @@ import lombok.Setter;
 @EntityListeners(AuditingEntityListener.class)
 
 public abstract class AuditModel implements Serializable {
-	private static final long serialVersionUID = 1L;
+    
+    private static final long serialVersionUID = 1L;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false, updatable = false)
